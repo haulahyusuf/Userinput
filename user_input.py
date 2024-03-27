10ef8058eb16f835347283c097330602f6f2812b
Haulah Yusuf <mukamihaulah@gmail.com>
2024-03-27 16:01:48 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Enter your location: ")
+print("Hello " + name + "!. You are " + age + " years old and you live in " + location + ".")
